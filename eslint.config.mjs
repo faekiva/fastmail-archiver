a45ea3978b11463ec164a636453f2004b2b1983e
kiva <git@kiva.lgbt>
2025-09-18 11:49:59 -0500

    console.log now ok

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,10 @@
 import eslintConfigCodely from "eslint-config-codely";
 
-export default [...eslintConfigCodely.full];
+export default [
+  ...eslintConfigCodely.full,
+  {
+    rules: {
+      "no-console": "off"
+    }
+  }
+];
